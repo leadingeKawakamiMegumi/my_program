5e895401954ca0d58954f37191f8675360daf80e
leadingeKawakamiMegumi <65448588+leadingeKawakamiMegumi@users.noreply.github.com>
2021-08-10 15:14:05 +0900

    Update image_utility.py

--- image_utility.py
+++ image_utility.py
@@ -20,7 +20,21 @@ def rotation_im(im,angle,scale):
     trans = cv2.getRotationMatrix2D(center, angle , scale) 
     
     image2 = cv2.warpAffine(im, trans, (w,h))
-    return(image2)
+    return(image2)]
+    
+def image_translation(im,shift_x,shift_y):
+    '''
+    input
+    -------------------------------
+    im:np.uint8
+    shiftx : #右（x方向）に移動したい量
+    shifty : #下（y方向）に移動したい量
+    '''
+    h,w = im.shape
+    shiftx = shift_x 
+    shifty = shift_y
+    mat = np.array([[1, 0, shiftx], [0, 1, shifty]], dtype=np.float32)
+    return(cv2.warpAffine(im, mat, (h,w)))
 
 
 def min_max_norm(image):
@@ -29,3 +43,14 @@ def min_max_norm(image):
     im_out = (image - a) / (b-a) *255
     im_out = im_out.astype(np.uint8)
     return(im_out)
+
+def make_cirkle_mask(im_size):
+    '''
+    input
+    -------------------------------
+    image: height(h) = width(w)
+    '''
+    im_size_half = int(im_size/2) 
+    print("circle_r",im_size_half) 
+    im_mask = np.zeros((im_size,im_size),int)
+    return(cv2.circle(im_mask,(im_size_half,im_size_half),im_size_half,255,-1))
